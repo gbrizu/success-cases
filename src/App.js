d516532a150f039138464fe3bdf7d0e41db1dfe4
Nicolas Gonzalez <nicolasgonzalezgutierrez2017@gmail.com>
2024-02-22 20:37:21 -0300

    selectListIndustry

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import MultipleSelect from './Componentes/selectListIndustry/selectListIndustry';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ marginRight: '100px' }}>Industry</p>
+          <MultipleSelect />
+        </div>
       </header>
     </div>
   );
