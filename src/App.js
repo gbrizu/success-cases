baa28888bfc269bc221ba2dc1d3ce6b79f9dbf1b
Mauro Barrales <bmauro25@gmail.com>
2024-02-26 20:39:53 -0300

    MultipleSelect deleted

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />} />
         <Route path="layout2" element={<MainLayout2 />} />
-       <MultipleSelect />
       </Routes>
     </BrowserRouter>
 
