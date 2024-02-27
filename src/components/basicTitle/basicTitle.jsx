e7b684755a078652a8e7c2ea67860ba36723320d
AgustinG130 <cafedeagustin@gmail.com>
2024-02-27 18:34:40 -0300

    Delete document.title

--- src/components/basicTitle/basicTitle.jsx
+++ src/components/basicTitle/basicTitle.jsx
@@ -1,10 +1,7 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import './basicTitle.css'
 
 export default function BasicTitle({basictitle}) {
-  useEffect(() => {
-    document.title = basictitle;
-  }, [basictitle]);
   return (
     <h1 className="basic-title">
     {basictitle}
