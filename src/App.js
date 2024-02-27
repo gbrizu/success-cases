1fdf4c89b280cdca28cd96ffd3e7480ce5a25dd2
Nicolas Gonzalez <nicolasgonzalezgutierrez2017@gmail.com>
2024-02-27 20:28:00 -0300

    App Restored

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainLayout from './components/MainLayout';
 import MainLayout2 from './components/MainLayout2';
-import MultipleSelect from './Componentes/selectListIndustry/selectListIndustry';
 
 function App() {
   const options = ['1','2'];
   return (
-    <MultipleSelect>MultipleSelect</MultipleSelect>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="layout2" element={<MainLayout2 />} />
+      </Routes>
+    </BrowserRouter>
 
 
   );
