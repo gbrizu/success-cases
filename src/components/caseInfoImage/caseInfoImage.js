be30df2e116c794159dd6fe661c138a5841bad49
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-02-27 18:58:14 -0300

    map

--- src/components/caseInfoImage/caseInfoImage.js
+++ src/components/caseInfoImage/caseInfoImage.js
@@ -1,10 +1,9 @@
 export default function CaseInfoImage({images})
 {
     return (
-        <div style={{background: "#7b7af5", textAlign: "center"}}>
-            <img src={images[0]} alt="" width={300}/>
-            <img src={images[1]} alt="" width={300}/>
-            <img src={images[2]} alt="" width={85} style={{position:"relative", float:"right", top:"580px", marginRight:"7px"}}/>
+        <div style={{background: "#7b7af5", textAlign: "center", width:"1500px", height:"710px", margin:"auto"}}>
+            {images.map((image, index) => (<img key={index} src={image} alt="" width={300}/>))}
+            <img src="https://i0.wp.com/khipu.ai/wp-content/uploads/2019/08/Globant-DarkBG-White.png?fit=768%2C151&ssl=1" id="globant-logo" alt="" width={85} style={{position:"relative", float:"right", top:"680px", marginRight:"7px"}}/>
         </div>
     )
 }
