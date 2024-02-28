9a9a5b25331ad9942d95799239514cb397293eac
FCorderi <felipe.cordeli@correo.ucu.edu.uy>
2024-02-28 18:49:57 -0300

    temporary placeholder

--- src/components/filterMainScreen/filterMainScreen.js
+++ src/components/filterMainScreen/filterMainScreen.js
@@ -1,36 +1,40 @@
-import { Select } from '@mui/base/Select';
-import { Option } from '@mui/base/Option';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { IconButton } from '@mui/material';
-import { option } from 'yargs';
+import Grid from "@mui/system/Unstable_Grid/Grid";
+import SelectListClients from "../selectListClients/selectListClients";
+import MultipleSelect from "../selectListIndustry";
+import SelectListProjectType from "../selectListProjectType/selectListProjectType";
+import "./filterMainScreen.css"
 
 function FilterMainScreen() {
-    const options = [
-        'one', 'two', 'three'
+
+    const clientsOptions = [
+        "one", "two", "three"
     ]
 
     return (
-        <div>
-            <div>
-                <p>Client</p>
-            </div>
+        <Grid container spacing={2}>
+            {/* <div className="wrapper"> */}
+            <Grid item xs={8}>
+                <SelectListClients options={clientsOptions}></SelectListClients>
+            </Grid>
 
-            <div>
-                <p>Industry</p>
-            </div>
-            <div>
-                <p>Project type</p>
-            </div>
-            <div>
+            <Grid item xs={8}>
+                <MultipleSelect></MultipleSelect>
+            </Grid>
+
+            <Grid item xs={8}>
+                <SelectListProjectType></SelectListProjectType>
+            </Grid>
+
+            <Grid item xs={8}>
                 <p>Date</p>
-            </div>
-            <div>
+            </Grid>
+
+            <Grid item xs={8}>
                 <p>Project contact *</p>
-            </div>
+            </Grid>
 
-            <ArrowDropDownIcon></ArrowDropDownIcon>
-
-        </div>
+            {/* </div> */}
+        </Grid>
     );
 }
 export default FilterMainScreen
