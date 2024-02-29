6e7cb8ee075f004c563a00a2580d828eb815d4ac
Nicolas Gonzalez <nicolasgonzalezgutierrez2017@gmail.com>
2024-02-29 01:27:46 -0300

    caseInfoImage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import DataTable from './components/dataTable/dataTable';
 import MainLayout from './components/MainLayout';
 import MainLayout2 from './components/MainLayout2';
 import EditionUtils from './components/editionModeUtils/editionModeUtils';
+import CaseInfo from './components/caseInfo/caseInfo';
 
 function App() {
   const options = ['1','2'];
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />} />
-        <Route path="layout2" element={<MainLayout2 />} />
-        <Route path="layout3" element={<EditionUtils />} />
-      </Routes>
-    </BrowserRouter>
+    <CaseInfo>CaseInfo</CaseInfo>
     
   );
 }
--- src/components/caseInfoImage/caseInfoImage.js
+++ src/components/caseInfoImage/caseInfoImage.js
@@ -1,9 +1,12 @@
-export default function CaseInfoImage({images})
-{
+export default function CaseInfoImage({ images }) {
     return (
-        <div style={{background: "#7b7af5", textAlign: "center", width:"1500px", height:"710px", margin:"auto"}}>
-            {images.map((image, index) => (<img key={index} src={image} alt="" width={300}/>))}
-            <img src="https://i0.wp.com/khipu.ai/wp-content/uploads/2019/08/Globant-DarkBG-White.png?fit=768%2C151&ssl=1" id="globant-logo" alt="" width={85} style={{position:"relative", float:"right", top:"680px", marginRight:"7px"}}/>
+        <div style={{ background: "#7b7af5", margin: "auto", position: "relative", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                {images.map((image, index) => (
+                    <img key={index} src={image} height={400} width={200} style={{ margin: "10px" }} />
+                ))}
+            </div>
+            <img src="https://i0.wp.com/khipu.ai/wp-content/uploads/2019/08/Globant-DarkBG-White.png?fit=768%2C151&ssl=1" alt="" width={85} style={{ position: "absolute", bottom: 0, right: 0, margin: "10px" }} />
         </div>
     )
 }
