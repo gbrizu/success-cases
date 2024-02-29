d3f4305f4bf249c32cd873dcf14a9dba4b7e84a5
yazminEspagnolo <104398358+yazminEspagnolo@users.noreply.github.com>
2024-02-29 19:33:30 -0300

    update mainLayout

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 function MainLayout() {
   return (
     <Container maxWidth='l' sx={{ bgcolor: '#cfe8fc' }}>
+
       <div id="TITLE">
-        <Box  sx={{ bgcolor: 'red', height: '12vh' }} />
+        <Box  sx={{ bgcolor: 'red', height: '8.1rem', marginBottom:'1.5rem'}} />
       </div>
+
       <div id="SEARCH BODY">
         <Grid container justifyContent="center" spacing={15} sx={{
           justifyContent: "flex-start"
@@ -19,36 +21,26 @@ function MainLayout() {
             <Box
               sx={{
                 bgcolor: 'green',
-                height: '50vh',
+                height: '20rem',
                 marginTop: '2rem'
 
               }}
             />
           </Grid>
 
-          <Grid key={2} item
-
-            sx={
-              {
-                width: '50%'
-              }
-            }>
-
-            <Link to="/layout2">
-              <Button variant="contained" size="large" sx={
-                {
-                  height: '5rem',
-                  marginTop: '15rem',
-                  marginLeft:'13rem',
-
-                }
-              }>Create new Success Case</Button></Link>
-
+          <Grid item xs={12} md={6}>
+            <Box sx={{ textAlign: 'center' }}>
+              <Link to="/layout2">
+                <Button variant="contained" size="large" sx={{ mt: { xs: '2rem', md: '15rem' } }}>
+                  Create new Success Case
+                </Button>
+              </Link>
+            </Box>
           </Grid>
         </Grid>
 
         <div id="TABLE">
-          <Box sx={{ bgcolor: 'yellow', height: '32.3vh', marginTop: '2rem' }} />
+          <Box sx={{ bgcolor: 'yellow', height: '21.8rem', mt: '2rem' }} />
         </div>
       </div>
     </Container>
