28fe8ac8677eaf5af4e31b5af46ba0b74a03ea07
FCorderi <felipe.cordeli@correo.ucu.edu.uy>
2024-02-29 18:29:15 -0300

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import './App.css';
 import DataTable from './components/dataTable/dataTable';
 import MainLayout from './components/MainLayout';
 import MainLayout2 from './components/MainLayout2';
-import FilterMainScreen from './components/filterMainScreen/filterMainScreen';
 import EditionUtils from './components/editionModeUtils/editionModeUtils';
 
 function App() {
   const options = ['1','2'];
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<MainLayout />} />
-    //     <Route path="layout2" element={<MainLayout2 />} />
-    //     <Route path="layout3" element={<EditionUtils />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <FilterMainScreen></FilterMainScreen>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="layout2" element={<MainLayout2 />} />
+        <Route path="layout3" element={<EditionUtils />} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
