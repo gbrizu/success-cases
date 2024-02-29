229faca90751e6d25fe8838961dd7c3c2c643371
yazminEspagnolo <104398358+yazminEspagnolo@users.noreply.github.com>
2024-02-29 18:16:27 -0300

    update mainLaayout

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -3,55 +3,30 @@ import { Link } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <Container maxWidth='l' sx={{ bgcolor: '#cfe8fc' }}>
-
+    <Container maxWidth='l' sx={{ bgcolor: '#cfe8fc', minHeight: '100vh' }}>
       <div id="TITLE">
-        <Box  sx={{ bgcolor: 'red', height: '12vh' }} />
-        
+        <Box sx={{ bgcolor: 'red', height: '12vh' }} />
       </div>
 
-      <div id="SEARCH BODY">
-        <Grid container justifyContent="center" spacing={15} sx={{
-          justifyContent: "flex-start"
-        }}>
-          <Grid key={1} item sx={
-            {
-              width: '50%'
-            }
-          }>
-            <Box
-              sx={{
-                bgcolor: 'green',
-                height: '50vh',
-                marginTop: '2rem'
-
-              }}
-            />
+      <div id="SEARCH BODY" sx={{ padding: '2rem' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ bgcolor: 'green', height: { xs: '20vh', md: '50vh' } }} />
           </Grid>
 
-          <Grid key={2} item
-
-            sx={
-              {
-                width: '50%'
-              }
-            }>
-
-            <Link to="/layout2">
-              <Button variant="contained" size="large" sx={
-                {
-                  height: '5rem',
-                  marginTop: '15rem',
-                  marginLeft:'13rem',
-
-                }
-              }>Create new Success Case</Button></Link>
-
+          <Grid item xs={12} md={6}>
+            <Box sx={{ textAlign: 'center' }}>
+              <Link to="/layout2">
+                <Button variant="contained" size="large" sx={{ mt: { xs: '2rem', md: '15rem' } }}>
+                  Create new Success Case
+                </Button>
+              </Link>
+            </Box>
           </Grid>
         </Grid>
 
         <div id="TABLE">
-          <Box sx={{ bgcolor: 'yellow', height: '32.3vh', marginTop: '2rem' }} />
+          <Box sx={{ bgcolor: 'yellow', height: '32.3vh', mt: '2rem' }} />
         </div>
       </div>
     </Container>
