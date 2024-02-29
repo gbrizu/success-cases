a1100354df3233055c309ea986f714dc6ebec351
yazminEspagnolo <104398358+yazminEspagnolo@users.noreply.github.com>
2024-02-29 18:33:52 -0300

    update mainLayout

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <Container maxWidth='l' sx={{ bgcolor: '#cfe8fc', minHeight: '100vh' }}>
+    <Container maxWidth='l' sx={{ bgcolor: '#cfe8fc', minHeight: '67.5rem' }}>
       <div id="TITLE">
-        <Box sx={{ bgcolor: 'red', height: '12vh' }} />
+        <Box sx={{ bgcolor: 'red', height: '8.1rem'}} />
       </div>
 
       <div id="SEARCH BODY" sx={{ padding: '2rem' }}>
         <Grid container spacing={3}>
           <Grid item xs={12} md={6}>
-            <Box sx={{ bgcolor: 'green', height: { xs: '20vh', md: '50vh' } }} />
+            <Box sx={{ bgcolor: 'green', height: { xs: '13.5rem', md: '18rem' } }} />
           </Grid>
 
           <Grid item xs={12} md={6}>
@@ -26,7 +26,7 @@ function MainLayout() {
         </Grid>
 
         <div id="TABLE">
-          <Box sx={{ bgcolor: 'yellow', height: '32.3vh', mt: '2rem' }} />
+          <Box sx={{ bgcolor: 'yellow', height: '21.8rem', mt: '2rem' }} />
         </div>
       </div>
     </Container>
