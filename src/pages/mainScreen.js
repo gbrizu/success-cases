927e2b5ef563dbb98eb7551b2af3c36647b14b20
BrunoCanepa <brunocanepa22@gmail.com>
2024-03-05 19:01:45 -0300

    a

--- src/pages/mainScreen.js
+++ src/pages/mainScreen.js
@@ -16,39 +16,8 @@ function MainLayout() {
 
   return (
     <Container maxWidth='l' sx={{ bgcolor: '#CFE8FC' }}>
-      <div id="TITLE">
-        <Box  sx={{ bgcolor: 'red', height: '8.1rem', marginBottom:'1.5rem'}} />
-      </div>
-      <div id="SEARCH BODY">
-        <Grid container spacing={2} sx={{
-          justifyContent: "center"
-        }}>
-          <Grid item xs={12} md={6}>
-            <Box
-              sx={{
-                bgcolor: 'green',
-                height: '20rem',
-                marginTop: '2rem',
-                width: { xs: '100%', md: '100%' },
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Box sx={{ textAlign: 'center', marginTop: '1rem' }}>
-              <Link to="/layout2">
-                <Button variant="contained" size="large">
-                  Create new Success Case
-                </Button>
-              </Link>
-            </Box>
-          </Grid>
-        </Grid>
-        <div id="TABLE">
-          <Box sx={{ bgcolor: 'yellow', height: '21.8rem', mt: '2rem' }} />
-        </div>
-      </div>
-      <CaseInfoScreen></CaseInfoScreen>*/
-      <CaseDetails client={client}></CaseDetails>
+      
+      <CaseDetails succesCase={client}></CaseDetails>
     </Container>
   );
 }
