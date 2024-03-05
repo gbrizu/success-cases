607c336d76a54c1a41dfed3af5acef5915d00e95
Guillermo Mourelle <131787225+mourelleguillermo@users.noreply.github.com>
2024-03-05 20:56:48 -0300

    buttons functional

--- src/components/caseDetails/caseDetails.js
+++ src/components/caseDetails/caseDetails.js
@@ -1,6 +1,7 @@
 import { Grid, Button, Container } from "@mui/material";
 import "../../App.css";
 import CaseDetailsInfo from "./caseDetailsInfo";
+import { Link } from "react-router-dom";
 
 export default function CaseDetails({ client }) {
   function backButton() {
@@ -12,13 +13,13 @@ export default function CaseDetails({ client }) {
   }
 
   const infoList = [
-    {label: "Client", info: "Info"}
+    { label: "Client", info: "Info" }
   ]
 
   return (
     <Container>
       <h1>Success Case Details</h1>
-      
+
       <CaseDetailsInfo label={"Client"} info={"Info"}></CaseDetailsInfo>
       <CaseDetailsInfo label={"Industry"} info={"Entretainment"}></CaseDetailsInfo>
       <CaseDetailsInfo label={"Project type"} info={"Development"}></CaseDetailsInfo>
@@ -27,20 +28,23 @@ export default function CaseDetails({ client }) {
       <CaseDetailsInfo label={"Project contact"} info={"milton.rodriguez@globant.com"}></CaseDetailsInfo>
       <CaseDetailsInfo label={"Avg. Team size"} info={"10"}></CaseDetailsInfo>
 
-      <Button
-        variant="contained"
-        onClick={backButton}
-        style={{ marginRight: "100px" }}
-      >
-        Back
-      </Button>
-      <Button
-        variant="contained"
-        onClick={nextButton}
-        style={{ marginLeft: "100px" }}
-      >
-        Next
-      </Button>
+      <Link to={"/"}>
+        <Button
+          variant="contained"
+          style={{ marginRight: "100px" }}
+        >
+          Back
+        </Button>
+      </Link>
+
+      <Link to="info">
+        <Button
+          variant="contained"
+          style={{ marginLeft: "100px" }}
+        >
+          Next
+        </Button>
+      </Link>
     </Container>
   );
 }
