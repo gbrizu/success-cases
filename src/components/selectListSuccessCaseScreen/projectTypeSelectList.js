fffef8a9f9251f129bdfa4e1bef12bef29d8a2a3
Leandro <leandrodcasaretto@gmail.com>
2024-03-06 19:43:27 -0300

    Cambios

---
+++ src/components/selectListSuccessCaseScreen/projectTypeSelectList.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Typography } from "@mui/material";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function ProjectTypeSelectList({ value, onChange, options }) {
+  const theme = useTheme();
+  return (
+    <div
+      style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}
+    >
+      <Typography variant="h6" style={{ marginRight: "4.8rem" }}>
+        Project Type
+      </Typography>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-name-label">
+          Select Project Type
+        </InputLabel>
+        <Select
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          multiple
+          value={value}
+          onChange={onChange}
+          input={<OutlinedInput label="Select Project Type" />}
+          MenuProps={MenuProps}
+        >
+          {options.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, value, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
