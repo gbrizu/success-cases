c0012131b8e75eedc6ef043398f2683cb0dd7f56
Matias Cabrera <matias.clavijo@koin.com.br>
2024-03-07 01:28:07 -0300

    last fix

--- src/components/containerCaseInfoDescription/caseInfoDescription.js
+++ src/components/containerCaseInfoDescription/caseInfoDescription.js
@@ -2,11 +2,15 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 
+function createMarkup(text) {
+  return {__html: text};
+}
+
 export default function FixedContainer({ title, textTest }) {
   return (
     <>
       <h1 style={{ margin: "0 0 10px" }}>{title}</h1>
-      <p style={{ margin: "0" }}>{textTest}</p>
+      <div dangerouslySetInnerHTML={createMarkup(textTest)} />
     </>
   );
 }
