75c23f18b2c65ed5803a29661e08c69646dc44dd
tomaspasto24 <60078552+tomaspasto24@users.noreply.github.com>
2024-03-07 00:47:51 -0300

    fix merge

--- src/context/process.context.js
+++ src/context/process.context.js
@@ -50,9 +50,9 @@ export default function ProcessContext({ children }) {
             improvements: { text: improvementsText, image: 'https://phantom-expansion.unidadeditorial.es/aaad57cd1e20cab29748a0375dcd0309/crop/0x110/1920x1191/resize/828/f/jpg/assets/multimedia/imagenes/2022/05/20/16530528329270.jpg' },
             technologie: { text: technologieText, image: 'https://phantom-expansion.unidadeditorial.es/aaad57cd1e20cab29748a0375dcd0309/crop/0x110/1920x1191/resize/828/f/jpg/assets/multimedia/imagenes/2022/05/20/16530528329270.jpg' },
         }
-        console.log(body)
+
         const res = await postSuccessCase(body);
-        console.log(res)
+
         if(res.message === 'El caso se ha creado correctamente.') {
             alert('El caso se ha creado correctamente.');
         }
