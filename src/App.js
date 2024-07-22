67738879207dc2bc74913090d5e93903158dd758
nicohf2022 <104432397+nicohf2022@users.noreply.github.com>
2024-07-22 19:33:59 -0300

    Listo

--- src/App.js
+++ src/App.js
@@ -11,26 +11,28 @@ import CaseDetails from './components/caseDetails/caseDetails';
 import CaseViewContext from "./context/casesView.context";
 import CaseViewProcessScreen from "./pages/caseViewProcessScreen";
 import ErrorScreen from "./pages/errorScreen";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 export const UserContext = createContext()
 
 function App() {
   return (
-    <ProcessContext>
-      <CaseViewContext>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainScreen />} />
-            <Route
-              path="NewSuccessCases"
-              element={<SuccessCaseProcessCreationScreen />}
-            />
-            <Route path="succesCase/:id" element={<CaseViewProcessScreen />} />
-            <Route path='*' element={<ErrorScreen/>} />
-          </Routes>
-        </BrowserRouter>
-      </CaseViewContext>
-    </ProcessContext>
+    <Auth0Provider domain="dev-0efequbnpda3f7au.us.auth0.com" clientId="T2Igi7TuY129hwxd84q3b8cs2cIqm5bg" redirectUri={window.location.origin}>
+      <ProcessContext>
+        <CaseViewContext>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<MainScreen />} />
+              <Route
+                path="NewSuccessCases"
+                element={<SuccessCaseProcessCreationScreen />}
+              />
+              <Route path="succesCase/:id" element={<CaseViewProcessScreen />} />
+              <Route path='*' element={<ErrorScreen />} />
+            </Routes>
+          </BrowserRouter>
+        </CaseViewContext>
+      </ProcessContext>
+    </Auth0Provider>
   );
 }
 
