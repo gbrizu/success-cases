c488aafbe6f9a2665fa01055c3560da5b387c04e
nicohf2022 <104432397+nicohf2022@users.noreply.github.com>
2024-07-29 19:07:48 -0300

    hola

--- src/components/selectListClients/selectListClients.jsx
+++ src/components/selectListClients/selectListClients.jsx
@@ -5,7 +5,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import './selectListClients.css';
+import './selectListClients.css'
 import { Typography } from '@mui/material';
 
 const ITEM_HEIGHT = 48;
@@ -19,10 +19,9 @@ const MenuProps = {
   },
 };
 
-export default function SelectListClients({ options = [], value, onChange }) {
+export default function SelectListClients({ options, value, onChange }) {
   const theme = useTheme();
   const [personName, setPersonName] = useState(value || []);
-  
   const handleChange = (event) => {
     const {
       target: { value },
@@ -45,11 +44,7 @@ export default function SelectListClients({ options = [], value, onChange }) {
           input={<OutlinedInput label="Name" />}
           MenuProps={MenuProps}
         >
-<<<<<<< HEAD
-          {options.map((item) => (
-=======
           {(options?.length > 0) && (options?.map((item) => (
->>>>>>> ddc4e849f3ff480eca86df812e47dc29876465a1
             <MenuItem
               key={item.id}
               value={item.id}
